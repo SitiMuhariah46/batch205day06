0e7b6ba3c96588b00dc57f48bdef0f2660ce601b
Siti Muhariah <sitimuhariah@yahoo.co.id>
2019-07-26 00:21:31 +0700

    Add files via upload

---
+++ day06/HurufBerurut/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HurufBerurut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tampung = Console.ReadLine().ToLower();
+
+            char[] kata = new char[tampung.Length];
+            kata = tampung.ToCharArray();
+
+                   
+                   //Console.WriteLine(kata);
+
+                   for (int i = 0; i <tampung.Length; i++)
+                   {
+                       for (int j = 0; j < i; j++)
+                       {
+                           if (kata[i] < kata[j])
+                           {
+                               char temp = kata[i];
+                               kata[i] = kata[j];
+                               kata[j] = temp;                              
+                           }
+                       }
+                   }
+
+                   for (int k = 0; k < tampung.Length; k++)
+                   {
+                       Console.Write(kata[k]);  
+                   }
+                            
+                   Console.ReadLine();
+        }
+    }
+}
---
+++ day06/angkaBerurut/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace angkaBerurut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tampung = Console.ReadLine().Split(' ');
+
+            int[] arrayTampung = new int[tampung.Length];
+
+            arrayTampung = Array.ConvertAll(tampung, int.Parse);
+
+            for (int i = tampung.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arrayTampung[j] > arrayTampung[j + 1])
+                    {
+                        int temp = arrayTampung[j];
+                        arrayTampung[j] = arrayTampung[j + 1];
+                        arrayTampung[j + 1] = temp;
+                    }
+                }
+            }
+            for (int ii = 0; ii < tampung.Length; ii++)
+                Console.Write(arrayTampung[ii] + " ");
+
+            Console.ReadLine();
+
+        }
+    }
+}
